1c9f58874b616955c6b97b30abaf352c3533591e
Trevor <tdkollin@edu.uwaterloo.ca>
2021-02-20 22:25:43 -0500

    Update audio splitting to split by speach activation

--- split_audio.py
+++ split_audio.py
@@ -1,48 +1,73 @@
 import sys
+from scipy.io import wavfile
 from pydub import AudioSegment
-from pydub.silence import split_on_silence
 from classifier.live_predictions import LivePredictions
+from google.cloud import speech, storage
+
+bucket_name = "sizu_audio"
+
+def upload_file(file_location: str, file_name: str):
+    print("Uploading file to GCP bucket... This might take a minute...")
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(file_name)
+    blob.upload_from_filename(file_location)
+
 
 def split_audio(file_location: str):
-    """
-    Split an audio .wav into segements by audio pauses
+    file_name = file_location.split("\\")[-1].split("/")[-1]
+    client = speech.SpeechClient()
+    upload_file(file_location, file_name)
+    gcs_uri = f"gs://{bucket_name}/{file_name}"
+    audio = speech.RecognitionAudio(uri=gcs_uri)
+    config = speech.RecognitionConfig(
+        encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
+        audio_channel_count=2,
+        language_code="en-US",
+        enable_word_time_offsets=True,
+    )
+    timestamps = []
+    speakers = []
+    print("Running speech to text... This might take a couple minutes...")
+    response = client.long_running_recognize(config=config, audio=audio).result(timeout=300)
+    for result in response.results:
+        words = result.alternatives[0].words
+        timestamps.append(words[0].start_time.total_seconds())
+        if len(words) >= 15:
+            for word in words[10:-5:10]:
+                timestamps.append(word.start_time.seconds)
+        print("Transcript: {}".format(result.alternatives[0].transcript))
+    print(timestamps)
 
-    Args:
-        file_location: the location of the .wav file to load
-    """
+    rate, data = wavfile.read(file_location)
+    timestamps = timestamps[1::]
+    start = 0
+    for idx, time in enumerate(timestamps):
+        end = int((rate * time))
+        print(start, end)
+        segment = data[start:end-1]
+        wavfile.write(f".//temp//segment{idx}.wav", rate, segment)
+        if (idx == len(timestamps) - 1):
+            wavfile.write(f".//temp//segment{idx}.wav", rate, data[end:])
+        start = end
 
-    try:
-        sound_file = AudioSegment.from_wav(file_location)
-    except Exception as err:
-        print(f"Can not find file at {file_location}")
-        raise err
-
-    # Define silence length and threshold
-    # silence_length = 200 # (400 ms)
-    # silence_threshold = -30 # Consider less than -8 dbfs to be "silent"
-    silence_length = 200 # (400 ms)
-    silence_threshold = -30 # Consider less than -8 dbfs to be "silent"
-
-    segments = split_on_silence(sound_file, silence_length, silence_threshold)
-
-    for idx, segment in enumerate(segments):
-        segment.export(f".//temp//segment{idx}.wav", format="wav")
-
-    serialize_audio(segments)
+    serialize_audio(len(timestamps), timestamps, speakers)
 
 
-def serialize_audio(segments):
-    total = 0
+def serialize_audio(num_files, timestamps, speakers):
+    timestamps = [0] + timestamps[:-1:]
     serialized_data = []
-    for idx, segment in enumerate(segments):
+    for idx in range(num_files):
+        print(f".//temp//segment{idx}.wav")
         live_prediction = LivePredictions(file=f".//temp//segment{idx}.wav")
         live_prediction.loaded_model.summary()
         emotion = live_prediction.make_predictions()
         serialized_data.append({
-            "start_time": total,
-            "emotion": emotion
+            "start_time": timestamps[idx],
+            "emotion": emotion,
+            "speaker": speakers[idx]
         })
-        total += segment.duration_seconds
+    print(serialized_data)
     return serialized_data
 
 
